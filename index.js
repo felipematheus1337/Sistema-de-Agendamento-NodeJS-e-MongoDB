cf829b5b4b0193974b3dfb5d3796d2ebd9413628
felipematheus1337 <lipehbr@gmail.com>
2022-10-25 20:24:05 -0300

    setup do projeto realizado

---
+++ index.js
@@ -0,0 +1,28 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json());
+
+
+app.set("view engine","ejs");
+
+
+mongoose.connect("mongodb://localhost:27017/agendamento",{
+    useNewUrlParser:true, useUnifiedTopology:true})
+
+
+
+app.get("/",(req,res) => {
+    res.send("oi")
+})
+
+
+app.listen(8080,() => {
+    console.log("Servidor rodando...")
+});
